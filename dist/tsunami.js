179da1086bf051f04a369cf6dd707edaae8a8d1b
patrickmatte <patrickmatte@gmail.com>
2016-08-17 16:04:02 -0400

    Added Rectangle.intersect

--- dist/tsunami.js
+++ dist/tsunami.js
@@ -3392,6 +3392,21 @@ tsunami.geom = tsunami.geom || {};
 		return rect.x + rect.width > this.x && rect.y + rect.height > this.y && rect.x < this.x + this.width && rect.y < this.y + this.height;
 	};
 
+	p.intersect = function(b) {
+		var a = this;
+		var x = Math.max(a.x, b.x);
+		var num1 = Math.min(a.x + a.width, b.x + b.width);
+		var y = Math.max(a.y, b.y);
+		var num2 = Math.min(a.y + a.height, b.y + b.height);
+		var result;
+		if (num1 >= x && num2 >= y) {
+			result = new tsunami.geom.Rectangle(x, y, num1 - x, num2 - y);
+		} else {
+			result = new tsunami.geom.Rectangle();
+		}
+		return result;
+	};
+	
 	p.equals = function(rect) {
 		return (this.x == rect.x && this.y == rect.y && this.width == rect.width && this.height == rect.height);
 	};
